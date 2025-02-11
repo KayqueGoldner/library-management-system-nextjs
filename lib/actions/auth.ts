c6f5195ee99200b6fa468fa9661e5f72de1bf537
Kayque Goldner <kayquegoldner@gmail.com>
2025-02-11 11:23:03 -0300

    setup upstash redis

--- lib/actions/auth.ts
+++ lib/actions/auth.ts
@@ -2,15 +2,23 @@
 
 import { eq } from "drizzle-orm";
 import { hash } from "bcryptjs";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 
 import { db } from "@/database/drizzle";
 import { users } from "@/database/schema";
 import { signIn } from "@/auth";
+import ratelimit from "@/lib/rate-limit";
 
 export const signInWithCredentials = async (
   params: Pick<AuthCredentials, "email" | "password">,
 ) => {
   const { email, password } = params;
+
+  const ip = (await headers()).get("x-forwarded-for") || "127.0.0.1";
+  const { success } = await ratelimit.limit(ip);
+
+  if (!success) return redirect("/too-fast");
 
   try {
     const result = await signIn("credentials", {
@@ -32,6 +40,11 @@ export const signInWithCredentials = async (
 
 export const signUp = async (params: AuthCredentials) => {
   const { email, fullName, password, universityCard, universityId } = params;
+
+  const ip = (await headers()).get("x-forwarded-for") || "127.0.0.1";
+  const { success } = await ratelimit.limit(ip);
+
+  if (!success) return redirect("/too-fast");
 
   // check if the user already exists
   const existingUser = await db
