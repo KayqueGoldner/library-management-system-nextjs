b05f2ecbb1501d26a76e4bfbfebf0c6ff5813147
Kayque Goldner <kayquegoldner@gmail.com>
2025-02-17 21:49:27 -0300

    user onboarding flow

--- lib/config.ts
+++ lib/config.ts
@@ -1,6 +1,7 @@
 const config = {
   env: {
     apiEndpoint: process.env.NEXT_PUBLIC_API_ENDPOINT!,
+    prodApiEndpoint: process.env.NEXT_PUBLIC_PROD_API_ENDPOINT!,
     imagekit: {
       privateKey: process.env.IMAGEKIT_PRIVATE_KEY!,
       publicKey: process.env.NEXT_PUBLIC_IMAGEKIT_PUBLIC_KEY!,
@@ -10,6 +11,8 @@ const config = {
     upstash: {
       redisUrl: process.env.UPSTASH_REDIS_URL!,
       redisToken: process.env.UPSTASH_REDIS_TOKEN!,
+      qstashUrl: process.env.QSTASH_URL!,
+      qstashToken: process.env.QSTASH_TOKEN!,
     },
   },
 };
